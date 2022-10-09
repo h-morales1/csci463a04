8fed951759e004b39d5294db3aaa45862ab62c98
Holzelh <spenser.vale.com@gmail.com>
2022-10-08 23:04:23 -0500

    all r-type instructions working

--- rv32i_decode.cpp
+++ rv32i_decode.cpp
@@ -11,6 +11,41 @@ std::string rv32i_decode::decode(uint32_t addr, uint32_t insn) {
     return render_lui(insn);
   case opcode_auipc:
     return render_auipc(insn);
+  case opcode_rtype:
+    switch (get_funct3(insn)) {
+    default:
+      return render_illegal_insn(insn);
+    case funct3_sll:
+      return render_rtype(insn, "sll");
+    case funct3_slt:
+      return render_rtype(insn, "slt");
+    case funct3_sltu:
+      return render_rtype(insn, "sltu");
+    case funct3_xor:
+      return render_rtype(insn, "xor");
+    case funct3_or:
+      return render_rtype(insn, "or");
+    case funct3_and:
+      return render_rtype(insn, "and");
+    case funct3_srx:
+      switch (get_funct7(insn)) {
+      default:
+        return render_illegal_insn(insn);
+      case funct7_srl:
+        return render_rtype(insn, "srl");
+      case funct7_sra:
+        return render_rtype(insn, "sra");
+      }
+    case funct3_add:
+      switch (get_funct7(insn)) {
+      default:
+        return render_illegal_insn(insn);
+      case funct7_add:
+        return render_rtype(insn, "add");
+      case funct7_sub:
+        return render_rtype(insn, "sub");
+      }
+    }
   }
   assert(0 && "unrecognized op, shouldnt be here"); // it should be impossible
                                                     // to ever get here
@@ -22,6 +57,29 @@ uint32_t rv32i_decode::get_rd(uint32_t insn) {
   uint32_t rd = (insn & 0x00000FFF) >> 7;
 
   return rd;
+}
+
+uint32_t rv32i_decode::get_funct3(uint32_t insn) {
+  uint32_t funct3 = (insn & 0x00007000) >> 12;
+
+  return funct3;
+}
+
+uint32_t rv32i_decode::get_funct7(uint32_t insn) {
+  uint32_t funct7 = (insn & 0xFE000000) >> 25;
+
+  return funct7;
+}
+
+uint32_t rv32i_decode::get_rs1(uint32_t insn) {
+  uint32_t rs1 = (insn & 0x000F8000) >> 15;
+  return rs1;
+}
+
+uint32_t rv32i_decode::get_rs2(uint32_t insn) {
+  uint32_t rs2 = (insn & 0x01F00000) >> 20;
+
+  return rs2;
 }
 
 // get imm_x funcs
@@ -71,6 +129,17 @@ std::string rv32i_decode::render_utype(uint32_t insn, const char *mnemonic) {
 
   os << render_mnemonic(mnemonic) << render_reg(rd) << ","
      << to_hex0x20((imm_u >> 12) & 0x0FFFFF);
+
+  return os.str();
+}
+
+std::string rv32i_decode::render_rtype(uint32_t insn, const char *mnemonic) {
+  uint32_t rd = get_rd(insn);
+
+  std::ostringstream os;
+
+  os << render_mnemonic(mnemonic) << render_reg(rd) << ","
+     << render_reg(get_rs1(insn)) << "," << render_reg(get_rs2(insn));
 
   return os.str();
 }
