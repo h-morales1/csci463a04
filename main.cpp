d2b23edd67fb7f45b796f9a8e830626a18f72781
Holzelh <spenser.vale.com@gmail.com>
2022-11-04 19:00:17 -0500

    added new comp options

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 //
 //***************************************************************************
 
+#include "cpu_single_hart.h"
 #include "memory.h"
 #include "rv32i_decode.h"
 #include <cstddef>
@@ -72,10 +73,26 @@ static void usage() {
  ********************************************************************************/
 int main(int argc, char **argv) {
   uint32_t memory_limit = 0x100; // default memory size is 0x100
+  bool show_disassembly = false;
+
+  memory mem(memory_limit);
+  cpu_single_hart cpu(mem);
 
   int opt;
-  while ((opt = getopt(argc, argv, "m:")) != -1) {
+  while ((opt = getopt(argc, argv, "dilrm:")) != -1) {
     switch (opt) {
+    case 'd': {
+      show_disassembly = true;
+    } break;
+    case 'i': {
+      cpu.set_show_instructions(true);
+    } break;
+    case 'l': { // TODO this requires parameters
+      std::cout << "exec limit" << std::endl;
+    } break;
+    case 'r': {
+      cpu.set_show_registers(true);
+    } break;
     case 'm': {
       std::istringstream iss(optarg);
       iss >> std::hex >> memory_limit;
@@ -89,13 +106,16 @@ int main(int argc, char **argv) {
   if (optind >= argc)
     usage(); // missing filename
 
-  memory mem(memory_limit);
-
   if (!mem.load_file(argv[optind]))
     usage();
 
-  disassemble(mem);
-  mem.dump();
+  if (show_disassembly)
+    disassemble(mem);
+
+  cpu.run(0); // TODO make this actually use exec_limit
+  cpu.reset();
+
+  //  mem.dump();
 
   return 0;
 }
